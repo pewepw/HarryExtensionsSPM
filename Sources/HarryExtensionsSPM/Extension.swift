d9aeb598f3ddf1da30c92046be088864ed007616
Harry <>
2025-04-04 10:34:07 +0800

    1.0.2

--- Sources/HarryExtensionsSPM/Extension.swift
+++ Sources/HarryExtensionsSPM/Extension.swift
@@ -87,32 +87,6 @@ extension UIViewController {
         return String(data: data, encoding: String.Encoding.utf8)
     }
     
-    public func isJailBroken() -> Bool {
-        if TARGET_IPHONE_SIMULATOR != 1 {
-            // Check 1 : existence of files that are common for jailbroken devices
-            if FileManager.default.fileExists(atPath: "/Applications/Cydia.app")
-                || FileManager.default.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib")
-                || FileManager.default.fileExists(atPath: "/bin/bash")
-                || FileManager.default.fileExists(atPath: "/usr/sbin/sshd")
-                || FileManager.default.fileExists(atPath: "/etc/apt")
-                || FileManager.default.fileExists(atPath: "/private/var/lib/apt/")
-                || UIApplication.shared.canOpenURL(URL(string:"cydia://package/com.example.package")!) {
-                return true
-            }
-            // Check 2 : Reading and writing in system directories (sandbox violation)
-            let stringToWrite = "Jailbreak Test"
-            do {
-                try stringToWrite.write(toFile:"/private/JailbreakTest.txt", atomically:true, encoding:String.Encoding.utf8)
-                //Device is jailbroken
-                return true
-            } catch {
-                return false
-            }
-        } else {
-            return false
-        }
-    }
-    
     public func showToast(message: String, font: UIFont = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium), textColor: UIColor = UIColor.white, backgroundColor: UIColor = UIColor.black) {
         let toastLabel = UILabel()
         toastLabel.textColor = textColor
